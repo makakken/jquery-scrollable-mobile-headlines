5570f59582f010d372bad926b36b14dc3d3cf057
Stephan <post@makakken.de>
2017-05-05 16:59:37 +0200

    Initial Commit

--- jquery.mobile-headlines.js
+++ jquery.mobile-headlines.js
@@ -1,57 +1,30 @@
-git@github.com:makakken/jquery-scrollable-mobile-headlines.git /* Front-Page headlines scrolling, responsive only */
-	if($(window).width() <= 992) {
-		/* Set Initial Containerheight */
-        $('#logobar-wrap').height( $('#logobar').outerHeight() );
-		$('#logobar').addClass("absolute");
+(function ( $ ) {
+	$.fn.myNewPlugin = function(options) {
+
+		var defaults = {
+			fixedClass: "fixed",
+			absoluteClass: "absolute"
+		};
+
+		var settings = $.extend( {}, defaults, options );
 
 		$(window).scroll(function() {
-			/* Check scroll Direction */
-			var ScrollDifference = $(window).scrollTop() - scrollTop;
-			scrollTop = $(window).scrollTop();
+			this.each(function() {
+				if( ($(this).offset().top + $(this).outerHeight() ) < $(window).scrollTop()) {
+					$(this).addClass(settings.fixedClass);
+				}
 
-			if(ScrollDifference < 0 ) {
-				$('#logobar')
-					.css('top',0)
-					.addClass("fixed")
-					.removeClass("absolute")
-					.data( 'toppos' , scrollTop );
-			} else {
-                var top = $('#logobar').data('toppos');
-				$('#logobar')
-                    .css('top',top)
-					.removeClass("fixed")
-					.addClass("absolute");
-			}
+				if($(this).offset().top >= $(window).scrollTop()) {
+					$(this).removeClass(settings.fixedClass);
+				}
 
-			titleLinks = $('.front-page').find('#target-groups div.active .target-list li a.active');
+				var bottom = $(this).parent().offset().top + $(this).parent().outerHeight() - $(this).outerHeight();
 
-			if(titleLinks.length > 0) {
-				titleLinks.each(function() {
-					if( ($(this).offset().top + $(this).outerHeight() ) < $(window).scrollTop()) {
-						showTitle = true;
-						titleText = $(this).text();
-						titleToggle = $(this).attr('data-target');
-					}
-
-					if($(this).offset().top >= $(window).scrollTop()) {
-						showTitle = false;
-					}
-
-					var bottom = $(this).parent().offset().top + $(this).parent().outerHeight() - $('.scrolling-header').outerHeight();
-
-					if(bottom < $(window).scrollTop()) {
-						showTitle = false;
-					}
-
-				});
-			}
-
-			if(showTitle) {
-				$('.scrolling-header').addClass('active');
-				$('.scrolling-header').find('a').html(titleText);
-				$('.scrolling-header').find('a').attr('data-target',titleToggle);
-			} else {
-				$('.scrolling-header').removeClass('active');
-			}
+				if(bottom < $(window).scrollTop()) {
+					$(this).addClass(settings.absoluteClass);
+					$(this).offsetTop($(window).scrollTop());
+				}
+			});
 		});
-	}
+	};
+}( jQuery ));
